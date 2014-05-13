a2b25869ea05059a79987440a1546dd7549389d3
tarikki <tarikkimeneenettiin@gmail.com>
2014-05-13 22:15:31 +0200

    finally something that works! :)

---
+++ src/package1/Command.java
@@ -0,0 +1,33 @@
+package package1;
+
+/**
+ * Command class
+ * Istances of this class represent a kind of command insert by users.
+ * @author Italian People
+ */
+
+
+public abstract class Command{
+	
+	private int value;
+	
+	public Command(int value){
+		this.value = value;
+		}
+	
+	/** 
+	 * @return command's value
+	 */
+	public int getValue(){
+		return this.value;
+		}
+		
+	/**
+	 * Set value to this instance of Command.
+	 * @param value
+	 */
+	public void setValue(int value) {
+		this.value = value;
+		}
+
+	}
---
+++ src/package1/World.java
@@ -0,0 +1,94 @@
+package package1;
+
+/**
+ * World class
+ * <p/>
+ * UK People
+ */
+public class World {
+
+    //ATTRIBUTES
+
+
+    /**
+     * Synchronizer
+     */
+    private Synchronizer synchronizer;
+
+    /**
+     * Snake
+     */
+    private Snake snake;
+
+    /**
+     * Food
+     */
+    private Food food;
+
+    /**
+     * Obstacle
+     */
+    private Obstacle obstacle;
+
+    private GameEngine gameEngine;
+
+    private Display display;
+
+    private Control control;
+
+
+    //CONSTRUCTORS
+
+    /**
+     * Empty constructor
+     * Initialize all the attributes by default
+     * and create a new World
+     */
+
+
+    /**
+     * Constructor
+     * Initialize all the attributes by default
+     * and create a new World
+     */
+    public World(Synchronizer synch) {
+        this.synchronizer = synch;
+        this.snake = new Snake(this.synchronizer);
+        this.food = new Food(this.synchronizer);
+        this.obstacle = new Obstacle(this.synchronizer);
+        this.display = new Display(this.synchronizer);
+        this.control = new Control(this.synchronizer);
+        this.food = new Food(this.synchronizer);
+        this.gameEngine = new GameEngine(this.snake, this.display, this.control, this.food, 5, this.synchronizer);
+
+
+
+    }
+
+
+    //METHODS
+
+
+    /**
+     * Method createNewGame
+     * Create a new world in which we places
+     * the obstacles, the Snake and the food each time in the area
+     *
+     * @param lvl the level chosen for the game
+     */
+    public void createNewGame(int lvl) {
+        this.obstacle = new Obstacle(lvl, this.synchronizer);    //create new obstacle placed in the area
+        this.gameEngine.startYourEngines();
+
+
+        //while the snake is alive the game continues
+        //and places the food randomly
+        //and the snake can move
+       /*while(snakeAlive){
+		   this.snake.Movet();
+		   this.food.placeFood();
+		   snakeAlive = this.synchronizer.getSnakeStillAlive();		//check each time is the snake is alive
+	   }*/
+    }
+
+}
